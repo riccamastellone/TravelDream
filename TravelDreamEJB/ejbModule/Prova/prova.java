e73ef18711994cb3e0ba785aeaa0377d6aaa5a8f
Sebastiano Mariani <seba0691@gmail.com>
2013-12-24 13:44:42 +0100

    aggiunto interfaccia rest a jsf 2
    Signed-off-by: Sebastiano Mariani <seba0691@gmail.com>

---
+++ TravelDreamEJB/ejbModule/Prova/prova.java
@@ -0,0 +1,18 @@
+package Prova;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class prova
+ */
+@Stateless
+public class prova implements provaLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public prova() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ TravelDreamEJBClient/ejbModule/Prova/provaLocal.java
@@ -0,0 +1,8 @@
+package Prova;
+
+import javax.ejb.Local;
+
+@Local
+public interface provaLocal {
+
+}

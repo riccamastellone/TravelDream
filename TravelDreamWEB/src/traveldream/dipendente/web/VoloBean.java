08ef0e7fdd9e07f85bab1b2a142d4b1c189473e6
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2014-01-07 20:59:36 +0100

    ordinamento funziona

--- TravelDreamWEB/src/traveldream/dipendente/web/VoloBean.java
+++ TravelDreamWEB/src/traveldream/dipendente/web/VoloBean.java
@@ -7,36 +7,32 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import org.primefaces.expression.impl.ThisExpressionResolver;
-
-import registrazione.client.UtenteDTO;
 import traveldream.dtos.VoloDTO;
 import traveldream.gestioneComponente.ComponenteMng;
 
-@ManagedBean(name="voloBean")
+@ManagedBean(name = "voloBean")
 @SessionScoped
 public class VoloBean {
 
 	@EJB
 	private ComponenteMng cmpMng;
-	
+
 	private VoloDTO volo;
-	
+
 	private String dataPartenza;
-	
+
 	private String dataArrivo;
-	
-	
-	public VoloBean(){
+
+	private ArrayList<VoloDTO> voli;
+
+	public VoloBean() {
 		this.volo = new VoloDTO();
-		//this.elencoVoli = cmpMng.getVoli();
+		// this.elencoVoli = cmpMng.getVoli();
 
 	}
-	
 
 	public VoloDTO getVolo() {
 		return volo;
@@ -45,73 +41,67 @@ public class VoloBean {
 	public void setVolo(VoloDTO volo) {
 		this.volo = volo;
 	}
-	
 
 	public String getDataPartenza() {
 		return dataPartenza;
 	}
 
-
 	public void setDataPartenza(String dataPartenza) {
 		this.dataPartenza = dataPartenza;
 	}
-
 
 	public String getDataArrivo() {
 		return dataArrivo;
 	}
 
-
 	public void setDataArrivo(String dataArrivo) {
 		this.dataArrivo = dataArrivo;
 	}
-	
-	
-	public String aggiungiVolo() throws ParseException{
+
+	public String aggiungiVolo() throws ParseException {
 		this.volo.setPartenza(this.converti(dataPartenza));
 		this.volo.setArrivo(this.converti(dataArrivo));
 		System.out.println(this.volo);
 		this.volo.printaDati();
 		cmpMng.salvaVolo(volo);
 		return "catalogo?faces-redirect=true";
-		
+
 	}
-	
-	public Date converti(String data) throws ParseException{
-		if (data.equals("")){
+
+	public Date converti(String data) throws ParseException {
+		if (data.equals("")) {
 			return null;
 		}
-		 SimpleDateFormat dateFormat = new SimpleDateFormat("mm/dd/yyyy");
-		 Date convertedDate = dateFormat.parse(data);
-		 return convertedDate;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("mm/dd/yyyy");
+		Date convertedDate = dateFormat.parse(data);
+		return convertedDate;
 	}
-	
 
-	
 	public ArrayList<VoloDTO> getVoli() {
-			return cmpMng.getVoli();
+		if (this.voli == null) {
+			this.voli = cmpMng.getVoli();
+		}
+		return this.voli;
 	}
-	
-	public void goToEdit(VoloDTO volo){
+
+	public void goToEdit(VoloDTO volo) {
 		this.volo = volo;
 		DateFormat formato = new SimpleDateFormat("MM/dd/yyyy");
 		this.dataArrivo = formato.format(this.volo.getArrivo());
 		this.dataPartenza = formato.format(this.volo.getPartenza());
-		
+
 	}
-	
+
 	public String editVolo() {
 		System.out.println("tato premuto");
 		cmpMng.aggiornaVolo(volo);
 		return "catalogo?faces-redirect=true";
 	}
-	
-	public void deleteVolo(VoloDTO volo){
+
+	public void deleteVolo(VoloDTO volo) {
 		System.out.println("tasto premuto");
 		cmpMng.deleteVolo(volo);
-		
-	}
-	
 
-	
+	}
+
 }

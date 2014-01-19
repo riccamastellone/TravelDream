c11d9a17c56fbda62732ced91d6e0a59037d7c85
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2014-01-19 16:47:58 +0100

    counter pacchetti

--- TravelDreamWEB/WebContent/resources/store/js/js-list.js
+++ TravelDreamWEB/WebContent/resources/store/js/js-list.js
@@ -7,29 +7,12 @@ jQuery(function() {
 
 
 //------------------------------
-//Counter
+//BG
 //------------------------------
 
 jQuery(function($) {
 	jQuery( "body" ).addClass("thebg");
-	$('.countprice').countTo({
-		from: 5,
-		to: 36,
-		speed: 1000,
-		refreshInterval: 50,
-		onComplete: function(value) {
-			console.debug(this);
-		}
-	});
-	$('.counthotel').countTo({
-		from: 1,
-		to: 53,
-		speed: 2000,
-		refreshInterval: 50,
-		onComplete: function(value) {
-			console.debug(this);
-		}
-	});			
+		
 });
 
 

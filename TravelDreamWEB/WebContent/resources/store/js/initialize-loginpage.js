b89f84ad679c1eeea74d25f5137d69cb7f031058
Sebastiano Mariani <seba0691@gmail.com>
2014-01-31 12:54:11 +0100

    aggiustato login js
    Signed-off-by: Sebastiano Mariani <seba0691@gmail.com>

--- TravelDreamWEB/WebContent/resources/store/js/initialize-loginpage.js
+++ TravelDreamWEB/WebContent/resources/store/js/initialize-loginpage.js
@@ -42,7 +42,12 @@ $(document).ready(function($) {
 				if(data=='<?xml version=\'1.0\' encoding=\'UTF-8\' ?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml">error</html>') {
 					errorMessage();
 				} else {
-					window.location = "store";
+					if (location.pathname == '/TravelDreamWEB/'){
+						window.location = "store";
+					}
+					else{
+						window.location = location.pathname;
+					}
 				}
 				
 			},
